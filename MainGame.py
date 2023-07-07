73f14b013ffc9613ce19bd2d25b8e5c65bf07e72
tedyvrb <137222872+tedyvrb@users.noreply.github.com>
2023-07-07 22:02:32 +0300

    update maingame.py to check function in score.py

--- MainGame.py
+++ MainGame.py
@@ -1,6 +1,10 @@
 from Live import load_game, welcome
+import Score
 
-print(welcome('Helena'))
+# print(welcome('Helena'))
 # return 2 parameters game_number and game_difficulty
-game_num, game_difficulty = load_game()
+# game_num, game_difficulty = load_game()
+score_file = 'Scores.txt'
+score = open(score_file, 'w').write('0')
+Score.add_score(5)
 
--- Score.py
+++ Score.py
@@ -2,13 +2,22 @@ def add_score(difficulty):
     # score_file_path = r'C:\Users\user\OneDrive - Qmasters\DevOps\Python\Python Project\WOG'
     score_file = 'Scores.txt'
     score = open(score_file, 'r').read()
-
     print(f'score check - {score}')
+    if score.isdigit():
+        score = int(score)
+    else:
+        score = 0
+
 
 
     points_of_wings = (difficulty * 3) + 5
-    new_score = int(score) + points_of_wings
-    open(score_file, 'w').write(new_score)
+    new_score = score + points_of_wings
+    open(score_file, 'w').write(str(new_score))
+
+    # check score file new value
+    new_value = open(score_file, 'r').read()
+    print(f'new value in score file is - {new_value}')
+    # ------------------------------------------------
 
     open(score_file).close()
 
