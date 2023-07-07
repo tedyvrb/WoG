91eaa77c48f40f6a3d3d35cff48657044f327f4d
tedyvrb <137222872+tedyvrb@users.noreply.github.com>
2023-07-07 21:13:01 +0300

    Update utils file add function screen_clean() add function bad_return_code update Score.py file add function add_score

--- Score.py
+++ Score.py
@@ -1,4 +1,19 @@
-def add_score(POINTS_OF_WINNING, DIFFICULTY):
-    return POINTS_OF_WINNING = (DIFFICULTY * 3) + 5
+def add_score(difficulty):
+    score_file_path = r'C:\Users\user\OneDrive - Qmasters\DevOps\Python\Python Project\WOG'
+    score_file = open(score_file_path, 'r')
+    score = score_file.read()
+    print(f'score check - {score}')
+
+
+    points_of_wings = (difficulty * 3) + 5
+    new_score = int(score) + points_of_wings
+    score_file = open(score_file_path, 'w')
+    score_file.write(new_score)
+
+    score_file.close()
+
+
+
+
 
 
--- Utils.py
+++ Utils.py
@@ -1,6 +1,6 @@
 import os
 
-SCORES_FILE_NAME = 'Score.py'
+SCORES_FILE_NAME = 'Scores.txt'
 BAD_RETURN_CODE = 500
 
 def screen_clean():
