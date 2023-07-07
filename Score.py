e5b797abf617752267e1cb0d1cf180f8c2092806
tedyvrb <137222872+tedyvrb@users.noreply.github.com>
2023-07-07 23:54:01 +0300

    add difficulty == 0, initialize file for the first time

--- Score.py
+++ Score.py
@@ -6,7 +6,7 @@ def add_score(difficulty):
     score_file = Utils.SCORES_FILE_NAME
 
     # If file not exist or removed created new file Scores
-    if not os.path.exists(score_file) or difficulty == 0:
+    if (not os.path.exists(score_file)) or difficulty == 0:
         with open(score_file, 'w') as file:
             file.write('0')
             file.close()
@@ -14,20 +14,21 @@ def add_score(difficulty):
     # Check Scores_file content
     score = open(score_file, 'r').read()
     print(f'Score check from the file is: {score}, difficulty is: {difficulty}')
-    if score.isdigit():
+    if score.isdigit() and score != 0:
         score = int(score)
+        points_of_wings = (difficulty * 3) + 5
+        new_score = score + points_of_wings
     else:
-        score = 0
+        new_score = 0
 
-    points_of_wings = (difficulty * 3) + 5
-    new_score = score + points_of_wings
+
     # check new_score
     print(f'New score check is: {new_score}')
     open(score_file, 'w').write(str(new_score))
 
     # check score file after adding new_value
-    new_value = open(score_file, 'r').read()
-    print(f'New value in score file is: {new_value}')
+    # new_value = open(score_file, 'r').read()
+    # print(f'New value in score file is: {new_value}')
     # ------------------------------------------------
 
     open(score_file).close()
