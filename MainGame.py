b92c26302566bf44fb455f1b68fbc07c6ddfe5db
tedyvrb <137222872+tedyvrb@users.noreply.github.com>
2023-07-07 22:19:48 +0300

    update add_score function

--- MainGame.py
+++ MainGame.py
@@ -4,7 +4,9 @@ import Score
 # print(welcome('Helena'))
 # return 2 parameters game_number and game_difficulty
 # game_num, game_difficulty = load_game()
+
 score_file = 'Scores.txt'
-score = open(score_file, 'w').write('0')
+# score = open(score_file, 'w').write('0')
+
 Score.add_score(5)
 
--- Score.py
+++ Score.py
@@ -1,22 +1,29 @@
+import os
+
 def add_score(difficulty):
     # score_file_path = r'C:\Users\user\OneDrive - Qmasters\DevOps\Python\Python Project\WOG'
     score_file = 'Scores.txt'
+
+    # If file not exist or removed created file Scores
+    if not os.path.exists(score_file):
+        with open(score_file,'w') as file:
+            file.write('0')
+
+    # Check Scores_file content
     score = open(score_file, 'r').read()
-    print(f'score check - {score}')
+    print(f'Score check is: {score}')
     if score.isdigit():
         score = int(score)
     else:
         score = 0
 
-
-
     points_of_wings = (difficulty * 3) + 5
     new_score = score + points_of_wings
     open(score_file, 'w').write(str(new_score))
 
-    # check score file new value
+    # check score file after adding new_value
     new_value = open(score_file, 'r').read()
-    print(f'new value in score file is - {new_value}')
+    print(f'New value in score file is: {new_value}')
     # ------------------------------------------------
 
     open(score_file).close()
