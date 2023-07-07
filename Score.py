6b2636a03609cb74c6e371ea4bc3bde5ebafa0d7
tedyvrb <137222872+tedyvrb@users.noreply.github.com>
2023-07-07 21:33:26 +0300

    update add_score function to most effective

--- Score.py
+++ Score.py
@@ -1,16 +1,16 @@
 def add_score(difficulty):
-    score_file_path = r'C:\Users\user\OneDrive - Qmasters\DevOps\Python\Python Project\WOG'
-    score_file = open(score_file_path, 'r')
-    score = score_file.read()
+    # score_file_path = r'C:\Users\user\OneDrive - Qmasters\DevOps\Python\Python Project\WOG'
+    score_file = 'Scores.txt'
+    score = open(score_file, 'r').read()
+
     print(f'score check - {score}')
 
 
     points_of_wings = (difficulty * 3) + 5
     new_score = int(score) + points_of_wings
-    score_file = open(score_file_path, 'w')
-    score_file.write(new_score)
+    open(score_file, 'w').write(new_score)
 
-    score_file.close()
+    open(score_file).close()
 
 
 
