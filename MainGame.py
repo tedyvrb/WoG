1e728c33b31d4e0ed5abff56c0d537cb75d96cf4
tedyvrb <137222872+tedyvrb@users.noreply.github.com>
2023-07-09 23:03:19 +0300

    update description in print function

--- MainGame.py
+++ MainGame.py
@@ -1,10 +1,6 @@
 from Live import load_game, welcome
-import Score
-import random
 
 # print(welcome('Helena'))
 # return 2 parameters game_number and game_difficulty
 game_num, game_difficulty = load_game()
-
-# Score.add_score(random.randint(1, 5))
 
